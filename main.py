970b4dbed3dbe2256d2e3d0265bd088aaccb3772
fernado <fernadocosta484@gmail.com>
2026-03-27 00:16:26 -0300

    feat: add tarefas de aluno 2, change how tokens are stored

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-# Nome: Luca Takemura Piccoli Usuario:
+# Nome: Luca Takemura Piccoli Usuario: MrJubiscreldo
 # Grupo: RA1 15 RA1
 
 def parseExpressao (linha, _tokens_):
   index = 0
   lenlinha = len(linha)
   while index < lenlinha:
+    # Se retornar index -1 algum erro foi detectado
     if index == -1:
-      estadoErro()
+      estadoErro(linha)
       return None
     else:
       letra = linha[index]
@@ -22,13 +23,13 @@ def parseExpressao (linha, _tokens_):
     elif letra in '+-*/%^':
       token, index = estadoOperador(linha, index)
       _tokens_.append(token)
-    elif letra in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+    elif letra in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
       token, index = estadoEspecial(linha, index)
       _tokens_.append(token)
     elif letra == '\n':
       break
     else:
-      estadoErro()
+      estadoErro(linha)
       return None
 
   return _tokens_
@@ -48,45 +49,135 @@ def estadoNumero(linha, index):
         numero += letra
         index += 1
       else:
-        return numero, -1
+        #Mais de um ponto dectectado
+        return "n" + numero, -1
     else:
-      return numero, index
+      return "n" + numero, index
 
-  return numero, index
+  return "n" + numero, index
 
 def estadoOperador(linha, index):
   operador = linha[index]
   if operador == '/' and linha[index + 1] == '/':
-    return operador + '/', index + 2
-  return operador, index+1
+    return 'o//', index + 2
+  return "o" + operador, index+1
 
 def estadoParenteses(linha, index):
   return linha[index], index+1
-  
+
 
 def estadoEspecial(linha, index):
   especial = linha[index]
   index += 1
 
   if especial == 'R' and linha[index] == 'E' and linha[index + 1] == 'S':
-    return 'RES', index + 2
+    return 'r', index + 2
   while index < len(linha):
     letra = linha[index]
-    if letra in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+    if letra in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
       especial += letra
       index += 1
     else:
-      return especial, index
+      return "m" + especial, index
 
-def estadoErro():
+def estadoErro(linha):
   print(f'Erro! expressao: {linha}')
 
-expressoestxt = 'teste1.txt'
+#Testar analisador lexico
+# def testarAL(tokens_list):
+#   for tokens in tokens_list:
+#     for token in tokens:
+      # todo: checar se token eh valido
 
-with open(expressoestxt, 'r') as expressoes:
-  tokens = []
+def resolver(n1, n2, operador):
+  if operador == "+":
+    return float(n1) + float(n2)
+  elif operador == "-":
+    return float(n1) - float(n2)
+  elif operador == "*":
+    return float(n1) * float(n2)
+  elif operador == "/":
+    return float(n1) / float(n2)
+  elif operador == "%":
+    return int(n1) % int(n2)
+  elif operador == "^":
+    return float(n1) ** float(n2)
+  else:
+    return int(n1) // int(n2)
+
+# Executar uma linha de tokens
+def executarExpressao(tokens, memoria, resultados):
+  pilha = []
+
+  for i, token in enumerate(tokens):
+    if token in "()":
+      continue
+    elif token[0] == "n":
+      pilha.append(token[1:])
+    elif token[0] == "o":
+      if len(pilha) < 2:
+        print("Erro: Falta tokens para operador")
+        return None
+      n1 = pilha.pop()
+      n2 = pilha.pop()
+      pilha.append(resolver(n1, n2, token[1:]))
+    elif token[0] == "m":
+      if len(pilha) < 1 or tokens[i+1][0] == "o":
+        mem = memoria.get(token[1:])
+        if mem == None:
+          mem = 0.0
+        pilha.append(mem)
+      else:
+        memoria[token[1:]] = pilha[-1]
+    elif token[0] == "r":
+      if len(pilha) < 1:
+        print("Erro: Falta tokens para RES")
+        return None
+      else:
+        res = int(pilha.pop())
+        if res < 1 or res >= len(resultados):
+          print("Erro: Resultado fora dos limites")
+          return None
+        else:
+          pilha.append(resultados[res]) 
+    else:
+      print("Erro: Token invalido")
+      return None
+  if len(pilha) == 1:
+    return pilha.pop()   
+  else:
+    print(f"Erro: Expressao invalida, tokens restantes: {len(pilha)}")
+    return None
+
+
+def executarExpressoes(tokens_list):
+  memoria = {}
+  resultados = []
+  for tokens in tokens_list:
+    resultados.append(executarExpressao(tokens, memoria, resultados))
+  return resultados
+
+
+def testarAFD():
+  expressoes =  ["((2 Y 3 +) (2 1 +)+)",
+                 "(((2 3 +) Y +) (2 1 +)+)",
+                 "(Y (2 1 +)+)",
+                 "(3.14 2.0 +)",
+                 "3 RES",]
+  tokens_list = []
   for linha in expressoes:
-    tokens.append(parseExpressao(linha, []))
+    tokens_list.append(parseExpressao(linha, []))
+  return executarExpressoes(tokens_list)
 
-  print(tokens)
+print(testarAFD())
 
+
+
+# expressoestxt = 'teste1.txt'
+
+# with open(expressoestxt, 'r') as expressoes:
+#   tokens_list = []
+#   for linha in expressoes:
+#     tokens_list.append(parseExpressao(linha, []))
+
+#   print(tokens_list)
